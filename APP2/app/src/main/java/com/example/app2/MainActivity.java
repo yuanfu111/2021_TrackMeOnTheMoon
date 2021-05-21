204d5f01a9c34ea46e7d23ae12d8e86949dd0804
yuanfu <y.fu-6@student.tudelft.nl>
2021-05-21 12:45:49 +0200

    APP2 version1

--- APP2/app/src/main/java/com/example/app2/MainActivity.java
+++ APP2/app/src/main/java/com/example/app2/MainActivity.java
@@ -29,8 +29,10 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
@@ -153,98 +155,54 @@ public class MainActivity extends Activity implements SensorEventListener {
             v.getBackground().setColorFilter(Color.GRAY, PorterDuff.Mode.ADD);
         }
     }
-
-//    private List<Float>  get_current_rss(List<String> chosen_macs) {
-//        //System.out.println(chosen_macs);
-//        scanWifi();
-////        System.out.println(scanned_MACs);
-////        System.out.println(scanned_RSS);
-////        System.out.println(chosen_macs);
-//        Float[] rss_values=new Float[chosen_macs.size()+1];//leave the last spot for the label
-//        Arrays.fill(rss_values,-100f);
-//
-//        // Compare scanned MACs against chosen MACs
-//        if(scanned_MACs.size()!=0) {
-//            /* for testing
-//            scanned_MACs.remove(1);
-//            scanned_MACs.add("c0:a0:bb:e9:87:87");
-//            */
-//            for (int i = 0; i < chosen_macs.size(); ++i) {
-//                for(int j=0;j<scanned_MACs.size();++j) {
-//                    /* for testing
-////                    System.out.println("chosen_macs(i)"+chosen_macs.get(i));
-////                    System.out.println("scanned_mac(j)"+scanned_MACs.get(j));
-//                     */
-//                    if ((chosen_macs.get(i)).equals(scanned_MACs.get(j))) {
-//                        rss_values[i] = scanned_RSS.get(j).floatValue();
-//
-//                    }
-//                }
-//            }
-//
-//            /* for testing
-//            System.out.println(scanned_MACs);
-//            System.out.println(scanned_RSS);
-//            System.out.println(chosen_macs);
-//            for(Float each:rss_values){
-//                System.out.println(each);
-//            }
-//             */
-//
-//            // Compare chosen MACs against scanned MACS
-////            for (int i = 0; i < chosen_macs.size(); ++i) {
-////                if (scanned_MACs.contains(chosen_macs.get(i)) == false) {
-////                    rss_values.set(i,-100f);
-////                }
-////            }
-//        }
-//        //List<Float> output=new ArrayList<Float>(Arrays.asList(rss_values));
-//        //output.add(-1f);
-//        return Arrays.asList(rss_values);
-//    }
-
-    //    private void scanWifi() {
-//        scanned_MACs.clear();
-//        scanned_RSS.clear();
-//        if(scan_results!=null) scan_results.clear();
-//        //registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-//        wifiManager.startScan();
-//        Toast.makeText(this, "Scanning WiFi ...", Toast.LENGTH_SHORT).show();
-//        scan_results = wifiManager.getScanResults();
-//        if(scan_results!=null) {
-//            for (ScanResult scanResult : scan_results) {
-//                scanned_MACs.add(scanResult.BSSID);
-//                scanned_RSS.add(scanResult.level);
-//            }
-////            System.out.println(scanned_MACs);
-////            System.out.println(scanned_RSS);
-//            scan_complete=true;
-//        }
-//        else{
-//
-//        }
-//        Toast.makeText(getApplicationContext(), "Complete scan!", Toast.LENGTH_SHORT).show();
-//    }
+    private void scanWifi() {
+        scanned_MACs.clear();
+        scanned_RSS.clear();
+        if(scan_results!=null) scan_results.clear();
+        //registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        wifiManager.startScan();
+        Toast.makeText(this, "Scanning WiFi ...", Toast.LENGTH_SHORT).show();
+        scan_results = wifiManager.getScanResults();
+        if(scan_results!=null) {
+            for (ScanResult scanResult : scan_results) {
+                scanned_MACs.add(scanResult.BSSID);
+                scanned_RSS.add(scanResult.level);
+            }
+        }
+        else{
+        }
+        Toast.makeText(getApplicationContext(), "Complete scan!", Toast.LENGTH_SHORT).show();
+    }
 //    BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
 //        @Override
 //        public void onReceive(Context context, Intent intent) {
 //            Toast.makeText(getApplicationContext(), "Complete scan!", Toast.LENGTH_SHORT).show();
 //            scan_results = wifiManager.getScanResults();
+//            System.out.println("Hello, the results are ready!");
+//            System.out.println("Size of the result: "+scan_results.size());
 //            unregisterReceiver(this);
-//
-//            for (ScanResult scanResult : scan_results) {
-//                scanned_MACs.add(scanResult.BSSID);
-//                scanned_RSS.add(scanResult.level);
-//            }
-//            start_scan = true;
-//            //start_scan=false;
 //        }
 //    };
+
     public void locate_me(View v) {
+        scanWifi();
+        for (ScanResult scanResult : scan_results) {
+            System.out.println("The results are being stored!");
+            scanned_MACs.add(scanResult.BSSID);
+            scanned_RSS.add(scanResult.level);
+            System.out.println(scanResult.BSSID);
+            System.out.println(scanResult.level);
+        }
+        System.out.println("All results are stored.");
+
+
         Float[] inital_prior=new Float[]{0.25f,0.25f,0.25f,0.25f};
         prior_serial=inital_prior;
         List<String> scaned_mac=new ArrayList<>();
-        List<Float> scaned_rss=new ArrayList<>();
+        List<Integer> scaned_rss=new ArrayList<>();
+
+
+
         scaned_mac.add("c0:a0:bb:e9:87:85");
         scaned_mac.add("28:d1:27:d8:0c:3e");
         scaned_mac.add("68:ff:7b:a9:67:94");//this one does not show in tha mac table. We need to clean it
@@ -260,64 +218,93 @@ public class MainActivity extends Activity implements SensorEventListener {
         scaned_mac.add("b0:4e:26:1d:d9:6b");
         scaned_mac.add("80:2a:a8:11:e2:3f");
         scaned_mac.add("00:4a:77:6a:6f:2f");
-        scaned_rss.add(-37f);
-        scaned_rss.add(-46f);
-        scaned_rss.add(-53f);
-        scaned_rss.add(-54f);
-        scaned_rss.add(-57f);
-        scaned_rss.add(-59f);
-        scaned_rss.add(-66f);
-        scaned_rss.add(-69f);
-        scaned_rss.add(-70f);
-        scaned_rss.add(-72f);
-        scaned_rss.add(-74f);
-        scaned_rss.add(-75f);
-        scaned_rss.add(-79f);
-        scaned_rss.add(-83f);
-        scaned_rss.add(-88f);
-        clean_scan_result(scaned_mac,scaned_rss);
-        for(int i=0;i<4;i++)
+        scaned_rss.add(-37);
+        scaned_rss.add(-46);
+        scaned_rss.add(-53);
+        scaned_rss.add(-54);
+        scaned_rss.add(-57);
+        scaned_rss.add(-59);
+        scaned_rss.add(-66);
+        scaned_rss.add(-69);
+        scaned_rss.add(-70);
+        scaned_rss.add(-72);
+        scaned_rss.add(-74);
+        scaned_rss.add(-75);
+        scaned_rss.add(-79);
+        scaned_rss.add(-83);
+        scaned_rss.add(-88);
+
+        //clean_scan_result(scaned_mac,scaned_rss);
+        //scaned_mac=scanned_MACs;
+        //scaned_rss=scanned_RSS;
+
+        Integer[] sorted_indexes=sort(scaned_rss);
+        for(int index:sorted_indexes)
+            System.out.println(scaned_rss.get(index));
+        int max_serial_itr=10;
+
+        for(int i=0;i<max_serial_itr;i++)
         {
-            posterior_serial=sense_serial(prior_serial,scaned_mac.get(i),scaned_rss.get(i));
+            System.out.println("Iteration: "+(i+1));
+            System.out.println(scaned_mac.get(sorted_indexes[i]));
+            System.out.println(scaned_rss.get(sorted_indexes[i]));
+            posterior_serial=sense_serial(prior_serial,scaned_mac.get(sorted_indexes[i]),scaned_rss.get(sorted_indexes[i]));
+            if(check_steady_state()) {
+                System.out.println("Steady State reached");
+                break;//if reaches steady state
+            }
+
+
             updata_serial_prior();
-//             for(Float each:posterior_serial)
-//             {
-//                 System.out.println(each);
-//             }
         }
+
         int pred_serial=getMaxIndex(posterior_serial);
         System.out.println(pred_serial);
+////
+////        List<Float[]> prior_parallel=new ArrayList<>();
+////        for(int i=0;i<scaned_rss.size();i++)
+////        {
+////            prior_parallel.add(new Float[]{0.25f,0.25f,0.25f,0.25f});
+////        }
+////
+////        int pred_parallel = sense_parallel(prior_parallel,scaned_mac,scaned_rss);
+//        //System.out.println(pred_parallel);
+//
+//        float prediction=pred_serial;
+//
+//        if (Math.abs(prediction - 0.0) < 0.1) {
+//            resetBg();
+//            CellA.setBackground(drawable_orange);
+//        }
+//        if (Math.abs(prediction - 1.0) < 0.1) {
+//            resetBg();
+//            CellB.setBackground(drawable_orange);
+//        }
+//
+//        if (Math.abs(prediction - 2.0) < 0.1) {
+//            resetBg();
+//            CellC.setBackground(drawable_orange);
+//        }
+//        if (Math.abs(prediction - 3.0) < 0.1) {
+//            resetBg();
+//            CellD.setBackground(drawable_orange);
+//        }
 
-        List<Float[]> prior_parallel=new ArrayList<>();
-        for(int i=0;i<scaned_rss.size();i++)
+    }
+    private boolean check_steady_state(){
+        boolean steady=true;
+        //if any big change happens, we should keep the iteration
+        for(int i=0;i<prior_serial.length;i++)
         {
-            prior_parallel.add(new Float[]{0.25f,0.25f,0.25f,0.25f});
+            float change_rate=Math.abs(prior_serial[i]-posterior_serial[i]);///prior_serial[i];
+            System.out.println("change rate: "+change_rate);
+            if(change_rate>0.000000001)
+            {
+                steady=false;
+            }
         }
-        //for(int i=0;i<10;i++)
-        //{
-        int pred_parallel = sense_parallel(prior_parallel,scaned_mac,scaned_rss);
-        //}
-        //System.out.println(pred_parallel);
-
-        float prediction=pred_parallel;
-
-        if (Math.abs(prediction - 0.0) < 0.1) {
-            resetBg();
-            CellA.setBackground(drawable_orange);
-        }
-        if (Math.abs(prediction - 1.0) < 0.1) {
-            resetBg();
-            CellB.setBackground(drawable_orange);
-        }
-
-        if (Math.abs(prediction - 2.0) < 0.1) {
-            resetBg();
-            CellC.setBackground(drawable_orange);
-        }
-        if (Math.abs(prediction - 3.0) < 0.1) {
-            resetBg();
-            CellD.setBackground(drawable_orange);
-        }
+        return steady;
+        //if(prior_serial[0]-posterior_serial[0])/
 
     }
     private void resetBg(){
@@ -327,9 +314,9 @@ public class MainActivity extends Activity implements SensorEventListener {
         CellD.setBackground(drawable_white);
     }
 
-    private  void clean_scan_result(List<String> raw_mac,List<Float> raw_rss){
+    private  void clean_scan_result(List<String> raw_mac,List<Integer> raw_rss){
         Iterator<String> it_mac=raw_mac.iterator();
-        Iterator<Float> it_rss=raw_rss.iterator();
+        Iterator<Integer> it_rss=raw_rss.iterator();
         while(it_mac.hasNext() && it_rss.hasNext())
         {
             String i=it_mac.next();
@@ -343,7 +330,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     public void updata_serial_prior(){
         prior_serial=posterior_serial;
     }
-    public Float[] sense_serial(Float[] prior,String rss_mac,Float rss)
+    public Float[] sense_serial(Float[] prior,String rss_mac,Integer rss)
     {
         Float[] posterior=new Float[numCells];
         int index_table =-1;
@@ -406,7 +393,7 @@ public class MainActivity extends Activity implements SensorEventListener {
         return minIndex;
     }
 
-    private int sense_parallel(List<Float[]> prior_parallel,List<String> rss_mac, List<Float> rss){
+    private int sense_parallel(List<Float[]> prior_parallel,List<String> rss_mac, List<Integer> rss){
         int num_mac=rss_mac.size();
 
         //List<Float[]> posteriors=new ArrayList<>();
@@ -430,4 +417,38 @@ public class MainActivity extends Activity implements SensorEventListener {
         }
         return getMaxIndex(vote);
     }
+
+//    private int[] sort_MACs (List<Integer> rss_values) {
+//        int[] ranks = new int[rss_values.size()];
+//        Arrays.fill(ranks, -101);
+//        List<Integer> rss_temp = new ArrayList<>();
+//        for (int i=0; i<rss_values.size(); ++i) {
+//            rss_temp.add(rss_values.get(i));
+//        }
+//        // 1. Find the max value, obtain index_max
+//        // 2. Update ranks[index_max]
+//        // 3. Set the current max value to -101f, go back to step 1
+//        for (int i=0; i<rss_values.size(); ++i) {
+//            Integer max = Collections.max(rss_temp);
+//            int index_max = rss_temp.indexOf(max);
+//            ranks[index_max] = i;
+//            rss_temp.set(index_max, -101);
+//        }
+//        return ranks;
+//    }
+
+    private Integer[] sort(List<Integer> input)
+    {
+        Integer[] indices = new Integer[input.size()];
+        for (int i = 0; i < indices.length; i++) {
+            indices[i] = i;
+        }
+        Arrays.sort(indices, new Comparator<Integer>() {
+            public int compare(Integer i1, Integer i2) {
+                return input.get(i2).compareTo(input.get(i1));
+            }
+        });
+        return indices;
+    }
+
 }
