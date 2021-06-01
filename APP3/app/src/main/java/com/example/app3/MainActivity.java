6bb51bc3bec9a1fc01c9ab9f59db6ed97e11abd8
yuanfu <y.fu-6@student.tudelft.nl>
2021-06-01 11:25:43 +0200

    setup listeners

--- APP3/app/src/main/java/com/example/app3/MainActivity.java
+++ APP3/app/src/main/java/com/example/app3/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.app3;
 
-
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
-
-public class MainActivity extends Activity implements OnClickListener {
+import android.hardware.SensorEventListener;
+public class MainActivity extends Activity implements SensorEventListener, OnClickListener {
 
     private Button button;
+    private SensorManager sensorManager;
+    private Sensor rotation_vector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +25,46 @@ public class MainActivity extends Activity implements OnClickListener {
 
         button=(Button)findViewById(R.id.button);
         button.setOnClickListener(this);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+
+            rotation_vector = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+            sensorManager.registerListener(this, rotation_vector,SensorManager.SENSOR_DELAY_NORMAL);
+        } else {
+
+        }
+    }
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, rotation_vector,SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 
     @Override
-    public void onClick(View v) {
-        if (v.getId()==R.id.button) {
-            Toast.makeText(getApplication(), "Click", Toast.LENGTH_SHORT).show();
+    public  void onClick(View v) {
+        if(v.getId()==R.id.button){
+            Toast.makeText(this,"Click",Toast.LENGTH_LONG).show();
         }
+
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float xValue = event.values[0];
+        float yValue = event.values[1];
+        float zValue = event.values[2];
+        //Log.d("Success", "x:"+xValue +";y:"+yValue+";z:"+zValue);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do nothing.
+    }
+
+
 }
